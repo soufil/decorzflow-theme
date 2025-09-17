e1d2d40cf90a5dcf72952ed8a878ef4080776c06
soufil <soufil@victorflow.com>
2025-09-17 12:50:27 +0530

    Initial commit with basic theme structure

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('toggleButton');
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
